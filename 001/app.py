3c8a603b1f899afb681a24aac66842cd205d6ff7
beta <beta@example.com>
2020-02-24 21:10:46 -0500

    주석 추가

--- 001/app.py
+++ 001/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
  
 @app.route('/')
-@app.route('/<int:num>')
+@app.route('/<int:num>')	# 주소창에 localhost:5000/3 처럼 3을 직접 입력해도 동작함
 def inputTest(num=None):
     return render_template('main.html', num=num)
 
